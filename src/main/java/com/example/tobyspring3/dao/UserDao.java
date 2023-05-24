ef6225de4c8763102e7f1ad4ff07915e70485647
ycyeon <ickite@naver.com>
2023-05-24 13:09:48 +0900

    feat: 메소드로 분리. ConnectionPool 분리

--- src/main/java/com/example/tobyspring3/dao/UserDao.java
+++ src/main/java/com/example/tobyspring3/dao/UserDao.java
@@ -2,40 +2,69 @@ package com.example.tobyspring3.dao;
 
 import com.example.tobyspring3.domain.User;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
 import java.util.Map;
 
 import static java.lang.System.getenv;
 
 public class UserDao {
-    public void add() throws ClassNotFoundException, SQLException {
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
         Map<String, String> env = getenv();
         String dbHost = env.get("DB_HOST");
         String dbUser = env.get("DB_USER");
         String dbPassword = env.get("DB_PASSWORD");
+
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection conn = DriverManager.getConnection(
                 dbHost, dbUser, dbPassword
         );
+        return conn;
+    }
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection conn = getConnection();
         PreparedStatement pstmt = conn.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-        pstmt.setString(1, "1");
-        pstmt.setString(2, "Chaeyeon");
-        pstmt.setString(3, "12345678");
+        pstmt.setString(1, user.getId());
+        pstmt.setString(2, user.getName());
+        pstmt.setString(3, user.getPassword());
 
         pstmt.executeUpdate();
         pstmt.close();
         conn.close();
     }
 
-    public User get() {
-        return new User();
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Connection conn = getConnection();
+        PreparedStatement pstmt = conn.prepareStatement("select id, name, password from users where id = ?");
+        pstmt.setString(1, id);
+        ResultSet rs = pstmt.executeQuery();
+        rs.next();
+
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("password"));
+
+        rs.close();
+        pstmt.close();
+        conn.close();
+
+        return user;
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         UserDao userDao = new UserDao();
-        userDao.add();
+        User user = new User();
+        user.setId("2");
+        user.setName("chaeyeon");
+        user.setPassword("1234");
+        //userDao.add(user);
+
+        User selectedUser = userDao.get("2");
+        System.out.println(selectedUser.getId());
+        System.out.println(selectedUser.getName());
+        System.out.println(selectedUser.getPassword());
+
     }
 }
